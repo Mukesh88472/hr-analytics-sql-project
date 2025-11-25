1b60fcd559d06d30397f319412023062f000a882
MUKESH CHOUDHARY <panaji890022@gmail.com>
2025-11-25 23:15:28 +0530

    Create 00_create_table.sql

---
+++ 00_create_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE hr_analytics(satisfaction_level DECIMAL(6,5),
+last_evaluation	DECIMAL(6,4),
+number_project INT,
+average_montly_hours DECIMAL(10,5),
+time_spend_company	DECIMAL(10,2),
+Work_accident INT,
+left_ INT,
+promotion_last_5years INT,
+Department VARCHAR(20),
+salary VARCHAR(15)
+)
